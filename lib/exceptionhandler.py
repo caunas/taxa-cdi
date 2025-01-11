2d558688457e5e50a39714a92abf5106f9c5d834
Cauan Nascimento <caunas1337@proton.me>
2025-01-11 19:57:02 -0300

    FIX: status alerta não retornava o prefixo de forma devida

--- lib/exceptionhandler.py
+++ lib/exceptionhandler.py
@@ -4,12 +4,15 @@ def exceptionhandler(message: str, status: str, prefix: str = "") -> None:
     try:
         if status == "alert":
             cor = "1;33"
-            prefix = f"\033[1;43m{prefix}\033[m"
+            prefixo = f"\033[1;43m{prefix}\033[m: "
         elif status == "error":
             cor = "1;31"
             prefixo = f"\033[1;41m{prefix}\033[m: "
         elif status == "ok":
             cor = "1;32"
+            prefixo = f"\033[1;42m{prefix}\033[m: "
+    
+        if prefix == "":
             prefixo = ""
 
         print(f"{prefixo}\033[{cor}m{message}\033[m")
