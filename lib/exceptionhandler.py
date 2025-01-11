1048f3530514f7121219ed7db0142a337f3db401
Cauan Nascimento <caunas1337@proton.me>
2025-01-11 16:37:42 -0300

    upload

---
+++ lib/exceptionhandler.py
@@ -0,0 +1,22 @@
+def exceptionhandler(message: str, status: str, prefix: str = "") -> None:
+    status = status.lower()
+    
+    try:
+        if status == "alert":
+            cor = "1;33"
+            prefix = f"\033[1;43m{prefix}\033[m"
+        elif status == "error":
+            cor = "1;31"
+            prefixo = f"\033[1;41m{prefix}\033[m: "
+        elif status == "ok":
+            cor = "1;32"
+            prefixo = ""
+
+        print(f"{prefixo}\033[{cor}m{message}\033[m")
+    
+    except UnboundLocalError as exc:
+       print(f"'{status}' isn't a valid status")
+    except TypeError as exc:
+        print("Invalid ARGS")
+    except Exception as exc:
+        print(exc)
